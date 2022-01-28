73c89b2a8f607623c25dd6dc313ee2e60192c58e
Oliver0804 <icetzsr@gmail.com>
2022-01-28 13:11:06 +0800

    更新Ble HID庫 加入免彈簧機構方式

--- main/blehid.ino
+++ main/blehid.ino
@@ -1,115 +1,98 @@
 
 
-#include <BleMouse.h>
-BleMouse bleMouse;
-
+#include <BleCombo.h>
 void bleHID_init() {
 
-  Serial.println("Starting BLE work!");
-  bleMouse.begin();
+  Serial.println("Starting work!");
+  Keyboard.begin();
+  Mouse.begin();
 }
+void bleComboHID(){
+   if(Keyboard.isConnected()) {
+    Serial.println("Sending 'Hello world'");
+    Keyboard.println("Hello World");
 
-void bleHID_control() {
-  if (bleMouse.isConnected()) {
-    Serial.println("Left click");
-    bleMouse.click(MOUSE_LEFT);
-    delay(500);
+    delay(1000);
+    Serial.println("Sending Enter key...");
+    Keyboard.write(KEY_RETURN);
 
-    Serial.println("Right click");
-    bleMouse.click(MOUSE_RIGHT);
-    delay(500);
+    delay(1000);
+  
+    Serial.println("Sending Play/Pause media key...");
+    Keyboard.write(KEY_MEDIA_PLAY_PAUSE);
 
-    Serial.println("Scroll wheel click");
-    bleMouse.click(MOUSE_MIDDLE);
-    delay(500);
+    delay(1000);
 
-    Serial.println("Back button click");
-    bleMouse.click(MOUSE_BACK);
-    delay(500);
-
-    Serial.println("Forward button click");
-    bleMouse.click(MOUSE_FORWARD);
-    delay(500);
-
-    Serial.println("Click left+right mouse button at the same time");
-    bleMouse.click(MOUSE_LEFT | MOUSE_RIGHT);
-    delay(500);
-
-    Serial.println("Click left+right mouse button and scroll wheel at the same time");
-    bleMouse.click(MOUSE_LEFT | MOUSE_RIGHT | MOUSE_MIDDLE);
-    delay(500);
-
-  }
-}
-
-void bleHID_move() {
-  if (bleMouse.isConnected()) {
+    Serial.println("Sending Ctrl+Alt+Delete...");
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press(KEY_LEFT_ALT);
+    Keyboard.press(KEY_DELETE);
+    delay(100);
+    Keyboard.releaseAll();
 
     unsigned long startTime;
 
-    Serial.println("Scroll up");
-    startTime = millis();
-    while (millis() < startTime + 2000) {
-      bleMouse.move(0, 0, 1);
-      delay(100);
-    }
-    delay(500);
-
-    Serial.println("Scroll down");
-    startTime = millis();
-    while (millis() < startTime + 2000) {
-      bleMouse.move(0, 0, -1);
-      delay(100);
-    }
-    delay(500);
-
-    Serial.println("Scroll left");
-    startTime = millis();
-    while (millis() < startTime + 2000) {
-      bleMouse.move(0, 0, 0, -1);
-      delay(100);
-    }
-    delay(500);
-
-    Serial.println("Scroll right");
-    startTime = millis();
-    while (millis() < startTime + 2000) {
-      bleMouse.move(0, 0, 0, 1);
-      delay(100);
-    }
-    delay(500);
-
     Serial.println("Move mouse pointer up");
     startTime = millis();
-    while (millis() < startTime + 2000) {
-      bleMouse.move(0, -1);
-      delay(100);
+    while(millis()<startTime+1000) {
+      Mouse.move(0,-1);
+      delay(5);
     }
-    delay(500);
+    Serial.println("Move mouse pointer left");
+    startTime = millis();
+    while(millis()<startTime+1000) {
+      Mouse.move(-1,0);
+      delay(5);
+    }
 
     Serial.println("Move mouse pointer down");
     startTime = millis();
-    while (millis() < startTime + 2000) {
-      bleMouse.move(0, 1);
-      delay(100);
+    while(millis()<startTime+1000) {
+      Mouse.move(0,1);
+      delay(5);
     }
-    delay(500);
-
-    Serial.println("Move mouse pointer left");
-    startTime = millis();
-    while (millis() < startTime + 2000) {
-      bleMouse.move(-1, 0);
-      delay(100);
-    }
-    delay(500);
 
     Serial.println("Move mouse pointer right");
     startTime = millis();
-    while (millis() < startTime + 2000) {
-      bleMouse.move(1, 0);
-      delay(100);
+    while(millis()<startTime+1000) {
+      Mouse.move(1,0);
+      delay(5);
     }
+    
+    Serial.println("Scroll Down");
+    Mouse.move(0,0,-1);
+
+    Serial.println("Left click");
+    Mouse.click(MOUSE_LEFT);
     delay(500);
 
+    Serial.println("Right click");
+    Mouse.click(MOUSE_RIGHT);
+    delay(500);
+
+    Serial.println("Scroll wheel click");
+    Mouse.click(MOUSE_MIDDLE);
+    delay(500);
+
+    Serial.println("Back button click");
+    Mouse.click(MOUSE_BACK);
+    delay(500);
+
+    Serial.println("Forward button click");
+    Mouse.click(MOUSE_FORWARD);
+    delay(500);
+
+    Serial.println("Click left+right mouse button at the same time");
+    Mouse.click(MOUSE_LEFT | MOUSE_RIGHT);
+    delay(500);
+
+    Serial.println("Click left+right mouse button and scroll wheel at the same time");
+    Mouse.click(MOUSE_LEFT | MOUSE_RIGHT | MOUSE_MIDDLE);
+    delay(500);
+
+
   }
-}
+  
+  Serial.println("Waiting 2 seconds...");
+  delay(2000);
+  }
--- main/main.ino
+++ main/main.ino
@@ -10,6 +10,14 @@
 bool blinkState = false;
 
 
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "I2Cdev.h"
+#include "MPU6050.h"
+MPU6050 accelgyro;
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+int16_t offset_ax, offset_ay , offset_az;
 
 
 void setup() {
@@ -18,13 +26,17 @@ void setup() {
 
   mpu6050_init();
   bleHID_init();
-
+  initOffset(5);
 }
 
 
 void loop() {
   rotary_loop();
-  mpu6050_read();
-  bleHID_control();
-
+  mpu6050_read(1);
+//  bleHID_control();
+   Serial.print(ax); Serial.print("\t");
 }
+
+void rat2hid(){
+  
+  }
